112dc494b6b8abfdb8d59acf9ef6a37247d86a74
FadilSadBoy20 <fadilkedua03@gmail.com>
2025-04-08 12:57:02 +0700

    Add files via upload
    Pekan 2 _Struktur Data_2411532013

---
+++ ArrayListGetElemen.java
@@ -0,0 +1,20 @@
+package Pekan1;
+
+import java.util.ArrayList;
+
+public class ArrayListGetElemen {
+
+	public static void main(String[] args) {
+				
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new
+		ArrayList<Integer>();
+		list.add(9);
+		list.add(5);
+		list.add(6);
+		System.out.println(list);
+		Integer n = list.get(1);
+		System.out.println("Pada Indeks ke 1 angkanya adalah:" + n);
+	}
+
+}
